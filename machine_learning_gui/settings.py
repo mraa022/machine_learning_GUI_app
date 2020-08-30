74308690408fa802f0809f7d15af1157e146e65c
mraa022 <42101911+mraa022@users.noreply.github.com>
2020-08-30 10:05:10 -0400

    -

--- machine_learning_gui/settings.py
+++ machine_learning_gui/settings.py
@@ -26,9 +26,9 @@ media_dir = os.path.join(BASE_DIR,'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'z5lxy-4cuch$hx1!s8emfpptsasq2^j@s9g^p1kax@srh2qi3!'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neural--network--playground.herokuapp.com','127.0.0.1']
 
 
 # Application definition

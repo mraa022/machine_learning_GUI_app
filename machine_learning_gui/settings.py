8e772d36e5183ae0c053c49015fbcaf38f356e84
adnan badri <adnanbadri99@gmail.com>
2020-10-10 23:49:06 -0400

    Update settings.py

--- machine_learning_gui/settings.py
+++ machine_learning_gui/settings.py
@@ -26,7 +26,7 @@ media_dir = os.path.join(BASE_DIR,'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'z5lxy-4cuch$hx1!s8emfpptsasq2^j@s9g^p1kax@srh2qi3!'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['neural--network--playground.herokuapp.com','127.0.0.1']
 
